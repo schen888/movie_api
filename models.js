dcf03f8be4859e397f0d8b05f2db84999117d5b3
schen888 <shuang.chen888@hotmail.com>
2022-10-27 17:11:38 +0200

    Correct the Birthday key in userSchema in models.js.

--- models.js
+++ models.js
@@ -24,7 +24,7 @@ let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
   Email: {type: String, required: true},
-  Birthday: Date,
+  Birthday: {Date},
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
