fc896780eee2834d01af7e72290d67ec67a118a0
schen888 <shuang.chen888@hotmail.com>
2022-10-27 17:24:19 +0200

    Delete the required in Birthday userSchema.

--- models.js
+++ models.js
@@ -24,7 +24,7 @@ let userSchema = mongoose.Schema({
   Username: {type: String, required: true},
   Password: {type: String, required: true},
   Email: {type: String, required: true},
-  Birthday: {type: Date, required: true},
+  Birthday: {type: Date},
   FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
 });
 
